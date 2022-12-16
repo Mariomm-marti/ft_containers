46ed6ee36dec9336d2e7b415d532899f0e9d02d6
mario <mmartin@42london.com>
2022-12-17 00:28:14 +0100

    wip(vector): Implementing capacity section

--- testers/test_vector.cpp
+++ testers/test_vector.cpp
@@ -1,4 +1,4 @@
-#include "../../vector.hpp"
+#include "../vector.hpp"
 #include <cassert>
 #include <iostream>
 #include <vector>
@@ -6,6 +6,13 @@
 void test_correct_max_size() {
   std::vector<int> a;
   ft::vector<int> b;
+
+  assert(a.max_size() == b.max_size());
+}
+
+void test_correct_max_size_complex() {
+  std::vector<std::vector<int>> a;
+  ft::vector<ft::vector<int>> b;
 
   assert(a.max_size() == b.max_size());
 }
--- vector.hpp
+++ vector.hpp
@@ -1,7 +1,4 @@
 #pragma once
-#include "vector_iterator.hpp"
-#ifndef FT_CONTAINERS_VECTOR_HPP_
-#define FT_CONTAINERS_VECTOR_HPP_
 
 #include <algorithm>
 #include <iostream> // TODO REMOVE
@@ -17,10 +14,6 @@ public:
   typedef typename allocator_type::const_reference const &const_reference;
   typedef typename allocator_type::pointer const *pointer;
   typedef typename allocator_type::const_pointer const *const_pointer;
-  typedef vector_iterator<value_type> *iterator; // TODO Replace with iterator
-  typedef vector_iterator<value_type> const *const_iterator;
-  typedef std::reverse_iterator<iterator> reverse_iterator;
-  typedef std::reverse_iterator<const_iterator> const_reverse_iterator;
   typedef std::ptrdiff_t difference_type;
   typedef std::size_t size_type;
 
@@ -31,21 +24,12 @@ private:
   size_type _capacity;
 
 public:
-  // explicit vector(allocator_type const &alloc = allocator_type())
-  //     : _vector(NULL), _allocator(alloc), _size(0), _capacity(0){};
+  explicit vector(allocator_type const &alloc = allocator_type())
+      : _vector(NULL), _allocator(alloc), _size(0), _capacity(0){};
   // explicit vector(size_type n, value_type const &val = value_type(),
   //                 allocator_type const &alloc = allocator_type())
   //     : _allocator(alloc), _size(n), _capacity(n){_allocator.};
-  explicit vector() {
-    _vector = new int[10];
-    _vector[0] = 42;
-    std::cout << _vector[0] << std::endl;
-  }
-
-  vector_iterator<T> begin(void) const { return vector_iterator<T>(_vector); }
 
   size_type max_size() const { return _allocator.max_size(); }
 }; // template vector
 } // namespace ft
-
-#endif
