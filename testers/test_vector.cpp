75aa60920fd70747b0b10f24fea1daba2cc64d33
mario <mmartin@42london.com>
2022-12-22 21:40:00 +0100

    wip(vector): started building constructors

--- testers/test_vector.cpp
+++ testers/test_vector.cpp
@@ -11,10 +11,17 @@ void test_correct_max_size() {
 }
 
 void test_correct_max_size_complex() {
-  std::vector<std::vector<int>> a;
-  ft::vector<ft::vector<int>> b;
+  std::vector<std::vector<int> > a;
+  ft::vector<ft::vector<int> > b;
 
   assert(a.max_size() == b.max_size());
+}
+
+void test_incorrect_max_size() {
+  std::vector<ft::vector<int> > a;
+  ft::vector<std::vector<int> > b;
+
+  assert(a.max_size() != b.max_size());
 }
 
 int main(void) { test_correct_max_size(); }
--- vector.hpp
+++ vector.hpp
@@ -1,12 +1,12 @@
+#ifndef FT_CONTAINERS_VECTOR_HPP_
+#define FT_CONTAINERS_VECTOR_HPP_
 #pragma once
 
-#include <algorithm>
-#include <iostream> // TODO REMOVE
-#include <iterator>
+#include "vector_iterator.hpp"
 #include <memory>
 
 namespace ft {
-template <class T, class Allocator = std::allocator<T> > class vector {
+template <class T, class Allocator = std::allocator<T>> class vector {
 public:
   typedef T value_type;
   typedef Allocator allocator_type;
@@ -14,6 +14,10 @@ public:
   typedef typename allocator_type::const_reference const &const_reference;
   typedef typename allocator_type::pointer const *pointer;
   typedef typename allocator_type::const_pointer const *const_pointer;
+  typedef ft::vector_iterator<value_type> iterator;
+  typedef ft::vector_iterator<value_type const> const_iterator;
+  // TODO Add reverse iterator based on the vector_iterator, std::enable_if
+  // TODO Add const reverse iterator
   typedef std::ptrdiff_t difference_type;
   typedef std::size_t size_type;
 
@@ -26,10 +30,15 @@ private:
 public:
   explicit vector(allocator_type const &alloc = allocator_type())
       : _vector(NULL), _allocator(alloc), _size(0), _capacity(0){};
-  // explicit vector(size_type n, value_type const &val = value_type(),
-  //                 allocator_type const &alloc = allocator_type())
-  //     : _allocator(alloc), _size(n), _capacity(n){_allocator.};
+  explicit vector(size_type n, value_type const &val = value_type(),
+                  allocator_type const &alloc = allocator_type())
+      : _allocator(alloc), _size(n), _capacity(n){_allocator.};
 
-  size_type max_size() const { return _allocator.max_size(); }
+  /*
+  ** capacity oriented functions
+  */
+  size_type size(void) const { return _size; }
+  size_type max_size(void) const { return _allocator.max_size(); }
 }; // template vector
 } // namespace ft
+#endif
