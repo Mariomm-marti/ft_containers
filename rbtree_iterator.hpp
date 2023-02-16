e2f40dbd628f323c697c95549f74d7481aa8df66
mario <mmartin@42london.com>
2023-02-16 23:24:01 +0100

    wip(rbtree): implemented node

--- rbtree_iterator.hpp
+++ rbtree_iterator.hpp
@@ -1,27 +1,90 @@
 #ifndef FT_CONTAINERS_RBTREE_ITERATOR_HPP_
 #define FT_CONTAINERS_RBTREE_ITERATOR_HPP_
-#include "iterator_traits.hpp"
-#include "rbtree.hpp"
-#include <functional>
-#include <iterator>
 #pragma once
 
+#include "rbtree_node.hpp"
+#include <cstddef>
+#include <functional>
+#include <iterator>
+
 namespace ft {
-template <class Key, class T, class Compare = std::less<T> >
-struct rbtree_iterator {
-  typedef rbtree_node<Key, T, Compare> node;
-  typedef typename std::bidirectional_iterator_tag iterator_category;
-  typedef typename ft::iterator_traits<node *>::value_type value_type;
-  typedef typename ft::iterator_traits<node *>::difference_type difference_type;
-  typedef typename ft::iterator_traits<node *>::pointer pointer;
-  typedef typename ft::iterator_traits<node *>::reference reference;
+template <class T, class Compare = std::less<T>> struct rbtree_iterator {
+  typedef std::bidirectional_iterator_tag iterator_category;
+  typedef ft::rbtree_node<T, Compare> value_type;
+  typedef std::ptrdiff_t difference_type;
+  typedef value_type *pointer;
+  typedef value_type &reference;
 
   rbtree_iterator(void) : _cursor(NULL){};
   rbtree_iterator(pointer const x) : _cursor(x){};
-  template <CopyIterator>
-  rbtree_iteartor()
+  template <class CopyIterator>
+  rbtree_iterator(rbtree_iterator<CopyIterator> const &x) : _cursor(x.base()){};
+  rbtree_iterator(rbtree_iterator const &x) : _cursor(x.base()){};
 
-      private : pointer _cursor;
+  template <class Iter>
+  rbtree_iterator &operator=(rbtree_iterator<Iter> const &x) {
+    _cursor = x.base();
+  };
+
+  template <class Iter> bool operator==(rbtree_iterator<Iter> const &x) const {
+    return _cursor == x.base();
+  };
+  template <class Iter> bool operator!=(rbtree_iterator<Iter> const &x) const {
+    return _cursor != x.base();
+  };
+
+  reference operator*(void) { return *_cursor; }
+  pointer operator->(void) { return _cursor; }
+
+  rbtree_iterator &operator++(void) {
+    pointer tmp;
+
+    if (_cursor == NULL)
+      return *this;
+    if (_cursor->parent == NULL) {
+
+      return *this;
+    }
+    if (_cursor->right != NULL) {
+      _cursor = _cursor->right;
+      while (_cursor->left != NULL)
+        _cursor = _cursor->left;
+      return *this;
+    }
+    tmp = _cursor->parent;
+    while (tmp != NULL && _cursor == tmp->right) {
+      _cursor = tmp;
+      tmp = tmp->parent;
+    }
+    _cursor = tmp;
+    return *this;
+  };
+
+  rbtree_iterator operator++(int) {
+    rbtree_iterator old = *this;
+    operator++();
+    return old;
+  };
+
+  rbtree_iterator &operator--(void) {
+    pointer tmp;
+
+    if (_cursor == NULL)
+      return *this;
+
+    return *this;
+  };
+
+  rbtree_iterator operator--(int) {
+    rbtree_iterator old = *this;
+    operator--();
+    return old;
+  };
+
+  pointer const base(void) const { return _cursor; }
+
+private:
+  pointer _cursor;
 };
 }; // namespace ft
 
---
+++ rbtree_node.hpp
@@ -0,0 +1,43 @@
+#ifndef FT_CONTAINERS_RBTREE_NODE_HPP_
+#define FT_CONTAINERS_RBTREE_NODE_HPP_
+#pragma once
+
+#include <functional>
+
+namespace ft {
+template <class T, class Compare = std::less<T>> struct rbtree_node {
+  typedef Compare key_compare;
+
+  T value;
+  rbtree_node *parent;
+  rbtree_node *left;
+  rbtree_node *right;
+
+  rbtree_node(void) : value(T()), parent(NULL), left(NULL), right(NULL){};
+  rbtree_node(rbtree_node const &copy)
+      : value(copy.value), parent(copy.parent), left(copy.left),
+        right(copy.right){};
+  rbtree_node(T value_, rbtree_node const *parent_ = NULL,
+              rbtree_node const *left_ = NULL, rbtree_node const *right_ = NULL)
+      : value(value_), parent(parent_), left(left_), right(right_){};
+  ~rbtree_node(void){};
+
+  rbtree_node &operator=(rbtree_node const &x) {
+    value = x.value;
+    parent = x.parent;
+    left = x.left;
+    right = x.right;
+  };
+
+  bool operator==(rbtree_node const &x) {
+    return !key_compare(value, x.value) && !key_compare(x.value, value);
+  };
+  bool operator!=(rbtree_node const &x) { return !(*this == x); };
+  bool operator<(rbtree_node const &x) { return key_compare(*this, x); };
+  bool operator>(rbtree_node const &x) { return x < *this; };
+  bool operator<=(rbtree_node const &x) { return !(x < *this); };
+  bool operator>=(rbtree_node const &x) { return !(*this < x); };
+};
+}; // namespace ft
+
+#endif
